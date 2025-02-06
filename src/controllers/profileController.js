0707dff84120a9bab8bb9b5030fc3e76b04b8c55
nibnualdi <nibnualdi@gmail.com>
2025-02-07 02:17:37 +0700

    feat: add update profile controller, route, & swagger

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -26,4 +26,35 @@ const getAProfile = async (req, res) => {
   }
 };
 
-export { getAProfile };
+const updateAProfile = async (req, res) => {
+  const { email, first_name, last_name } = req.body;
+  console.log(email, "email profile")
+
+  try {
+    await sequelize.query("UPDATE memberships SET first_name = ?, last_name = ? WHERE email = ?", {
+      replacements: [first_name, last_name, email],
+      type: QueryTypes.UPDATE,
+    });
+
+    const results = await sequelize.query("SELECT email,first_name,last_name,profile_image FROM memberships WHERE email = ?", {
+      replacements: [email],
+      type: QueryTypes.SELECT,
+    });
+
+    res
+      .status(200)
+      .json({ status: 200, message: "Sukses", data: results[0] });
+  } catch (error) {
+    if (error.name === "JsonWebTokenError")
+      return res.status(401).json({
+        status: 108,
+        message: "Token tidak valid atau kadaluwarsa",
+        data: null,
+      });
+
+    // default error
+    return res.status(500).json({ status: 500, message: "Internal Server Error", data: null });
+  }
+};
+
+export { getAProfile, updateAProfile };
--- src/routers/profile.js
+++ src/routers/profile.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAProfile } from "../controllers/profileController.js";
+import { getAProfile, updateAProfile } from "../controllers/profileController.js";
 import { accessTokenOnly } from "../middleware/authorization.js";
 
 const router = express.Router();
 
 router.get("/profile", [accessTokenOnly], getAProfile);
+router.put("/profile/update", [accessTokenOnly], updateAProfile);
 
 export default router;
