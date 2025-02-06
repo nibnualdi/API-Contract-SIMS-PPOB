8d91d88039e9241fe35a4e0740cd8dc422563e8b
nibnualdi <nibnualdi@gmail.com>
2025-02-07 02:39:16 +0700

    feat: add upload, profile image controller, route

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,18 +1,20 @@
 import { QueryTypes } from "sequelize";
 import sequelize from "../config/connection.js";
+import multer from "multer";
 
 const getAProfile = async (req, res) => {
   const { email } = req.body;
-  console.log(email, "email profile")
+  console.log(email, "email profile");
 
   try {
-    const results = await sequelize.query("SELECT email,first_name,last_name,profile_image FROM memberships WHERE email = ?", {
-      replacements: [email],
-      type: QueryTypes.SELECT,
-    });
-    res
-      .status(200)
-      .json({ status: 200, message: "Sukses", data: results[0] });
+    const results = await sequelize.query(
+      "SELECT email,first_name,last_name,profile_image FROM memberships WHERE email = ?",
+      {
+        replacements: [email],
+        type: QueryTypes.SELECT,
+      }
+    );
+    res.status(200).json({ status: 200, message: "Sukses", data: results[0] });
   } catch (error) {
     if (error.name === "JsonWebTokenError")
       return res.status(401).json({
@@ -28,7 +30,7 @@ const getAProfile = async (req, res) => {
 
 const updateAProfile = async (req, res) => {
   const { email, first_name, last_name } = req.body;
-  console.log(email, "email profile")
+  console.log(email, "email profile");
 
   try {
     await sequelize.query("UPDATE memberships SET first_name = ?, last_name = ? WHERE email = ?", {
@@ -36,14 +38,15 @@ const updateAProfile = async (req, res) => {
       type: QueryTypes.UPDATE,
     });
 
-    const results = await sequelize.query("SELECT email,first_name,last_name,profile_image FROM memberships WHERE email = ?", {
-      replacements: [email],
-      type: QueryTypes.SELECT,
-    });
+    const results = await sequelize.query(
+      "SELECT email,first_name,last_name,profile_image FROM memberships WHERE email = ?",
+      {
+        replacements: [email],
+        type: QueryTypes.SELECT,
+      }
+    );
 
-    res
-      .status(200)
-      .json({ status: 200, message: "Sukses", data: results[0] });
+    res.status(200).json({ status: 200, message: "Sukses", data: results[0] });
   } catch (error) {
     if (error.name === "JsonWebTokenError")
       return res.status(401).json({
@@ -57,4 +60,68 @@ const updateAProfile = async (req, res) => {
   }
 };
 
-export { getAProfile, updateAProfile };
+const port = process.env.PORT || 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/images");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      new Date().toISOString().replace(/:/g, "-") + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + "screenshot" + "_" + file.originalname);
+  },
+});
+
+const multerUpload = multer({ storage });
+
+const updateProfileImage = (req, res) => {
+  const { email } = req.body;
+  multerUpload.single("file")(req, res, async (error) => {
+    if (error) {
+      res.status(400).json({
+        message: "Gagal mengupload file!",
+        file: req.file,
+      });
+      return;
+    }
+    if (!req.file) {
+      res.status(400).json({
+        message: "Pastikan file sudah terpilih!",
+        file: req.file,
+      });
+      return;
+    }
+    try {
+      const path = `${req.file?.destination}/${req.file?.filename}`;
+      const url = `http://localhost:${port}/${path}`;
+
+      await sequelize.query("UPDATE memberships SET profile_image = ? WHERE email = ?", {
+        replacements: [url, email],
+        type: QueryTypes.UPDATE,
+      });
+
+      const results = await sequelize.query(
+        "SELECT email,first_name,last_name,profile_image FROM memberships WHERE email = ?",
+        {
+          replacements: [email],
+          type: QueryTypes.SELECT,
+        }
+      );
+
+      res.status(200).json({ status: 200, message: "Sukses", data: results[0] });
+    } catch (error) {
+      if (error.name === "JsonWebTokenError")
+        return res.status(401).json({
+          status: 108,
+          message: "Token tidak valid atau kadaluwarsa",
+          data: null,
+        });
+
+      // default error
+      return res.status(500).json({ status: 500, message: "Internal Server Error", data: null });
+    }
+  });
+};
+
+export { getAProfile, updateAProfile, updateProfileImage };
--- src/routers/profile.js
+++ src/routers/profile.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAProfile, updateAProfile } from "../controllers/profileController.js";
+import { getAProfile, updateAProfile, updateProfileImage } from "../controllers/profileController.js";
 import { accessTokenOnly } from "../middleware/authorization.js";
 
 const router = express.Router();
 
 router.get("/profile", [accessTokenOnly], getAProfile);
 router.put("/profile/update", [accessTokenOnly], updateAProfile);
+router.put("/profile/image", [accessTokenOnly], updateProfileImage);
 
 export default router;
