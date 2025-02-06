e9e455613d1a7db9bfc95dfb51dd19663fc8909d
nibnualdi <nibnualdi@gmail.com>
2025-02-06 22:41:50 +0700

    add swagger config & add membership module in swagger

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ console.log("[server]: Wait a moment!");
 import express from "express";
 import dotenv from "dotenv";
 import routers from "./routers/router.js";
+import swagger from "./swagger/config.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
 app.use("/", routers);
+app.use("/", swagger);
 
 app.get("/", (_, res) => {
   res.send("Welcome!!");
---
+++ src/swagger/config.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const router = Router();
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "API Contract SIMS PPOB",
+      version: "0.1.0",
+      description:
+        "API Test",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Ibnu Aldi Nugroho",
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}/`,
+      },
+    ],
+  },
+  apis: ["src/swagger/*.yml"],
+};
+
+const specs = swaggerJsdoc(options);
+
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+export default router;
