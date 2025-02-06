9c19ba08691d92d6df48a5e28a0d1f649480a2a4
nibnualdi <nibnualdi@gmail.com>
2025-02-07 00:47:01 +0700

    feat: add middleware

---
+++ src/middleware/authorization.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+
+const accessTokenOnly = (req, res, next) => {
+  const accessToken = req.headers.authorization?.split(" ")[1] || "";
+  const privateKey = process.env.PRIVATE_KEY;
+
+  try {
+    const jwtRes = jwt.verify(accessToken, privateKey);
+    req.body.email = jwtRes.email;
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError")
+      return res.status(401).json({
+        status: 108,
+        message: "Token tidak valid atau kadaluwarsa",
+        data: null,
+      });
+
+    // default error
+    return res.status(500).json({ status: 500, message: "Internal Server Error", data: null });
+  }
+};
+
+export { accessTokenOnly };
