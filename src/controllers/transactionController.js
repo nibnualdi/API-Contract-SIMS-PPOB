040c6dabbaa36b328adc431baa94e48ff19b2e3a
nibnualdi <nibnualdi@gmail.com>
2025-02-07 20:59:08 +0700

    feat: add transaction history controller & router

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -96,4 +96,34 @@ const transaction = async (req, res) => {
   }
 };
 
-export { transaction };
+const getTransactionHistoryByEmail = async (req, res) => {
+  const { email, offset, limit } = req.body;
+
+  try {
+    const results = await sequelize.query(
+      "SELECT * FROM invoices i JOIN memberships m ON i.membership_id = m.id LIMIT ? OFFSET ?",
+      {
+        replacements: [offset, limit],
+        type: QueryTypes.SELECT,
+      }
+    );
+    res.status(200).json({
+      status: 0,
+      message: "Get History Berhasil",
+      data: { offset, limit, records: results },
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.name === "JsonWebTokenError" || error.name === "TokenExpiredError")
+      return res.status(401).json({
+        status: 108,
+        message: "Token tidak valid atau kadaluwarsa",
+        data: null,
+      });
+
+    // default error
+    return res.status(500).json({ status: 500, message: "Internal Server Error", data: null });
+  }
+};
+
+export { transaction, getTransactionHistoryByEmail };
--- src/routers/transaction.js
+++ src/routers/transaction.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { transaction } from "../controllers/transactionController.js";
+import { transaction, getTransactionHistoryByEmail } from "../controllers/transactionController.js";
 import { accessTokenOnly } from "../middleware/authorization.js";
 
 const router = express.Router();
 
 router.post("/transaction", [accessTokenOnly], transaction);
+router.get("/transaction/history", [accessTokenOnly], getTransactionHistoryByEmail);
 
 export default router;
