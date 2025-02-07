34e97a222598eb33ea9f3896e1c94d80c98d6fcf
nibnualdi <nibnualdi@gmail.com>
2025-02-07 21:16:29 +0700

    feat: handle topup invoice

--- src/controllers/balanceController.js
+++ src/controllers/balanceController.js
@@ -1,5 +1,6 @@
 import { QueryTypes } from "sequelize";
 import sequelize from "../config/connection.js";
+import { v4 as uuidv4 } from "uuid";
 
 const getABalanceByEmail = async (req, res) => {
   const { email } = req.body;
@@ -30,6 +31,8 @@ const getABalanceByEmail = async (req, res) => {
 
 const topup = async (req, res) => {
   const { email, top_up_amount } = req.body;
+  const uuid = uuidv4();
+  const today = new Date();
 
   if (top_up_amount < 0)
     return res.status(400).json({
@@ -46,14 +49,37 @@ const topup = async (req, res) => {
         type: QueryTypes.UPDATE,
       }
     );
+    const resultsTransactionType = await sequelize.query(
+      "SELECT id,name FROM transaction_types WHERE name = ?",
+      {
+        replacements: ["TOPUP"],
+        type: QueryTypes.SELECT,
+      }
+    );
     const results = await sequelize.query(
-      "SELECT balance FROM balances b JOIN memberships m ON b.membership_id = m.id WHERE m.email = ?",
+      "SELECT balance,membership_id FROM balances b JOIN memberships m ON b.membership_id = m.id WHERE m.email = ?",
       {
         replacements: [email],
         type: QueryTypes.SELECT,
       }
     );
-    res.status(200).json({ status: 200, message: "Sukses", data: results[0] });
+    await sequelize.query(
+      "INSERT INTO `invoices` (`id`, `membership_id`, `transaction_type_id`, `description`, `total_amount`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      {
+        replacements: [
+          uuid,
+          results[0].membership_id,
+          resultsTransactionType[0].id,
+          "Top Up balance",
+          top_up_amount,
+          today,
+          today,
+        ],
+        type: QueryTypes.INSERT,
+      }
+    );
+
+    res.status(200).json({ status: 200, message: "Sukses", data: results[0].balance });
   } catch (error) {
     console.log(error);
     if (error.name === "JsonWebTokenError" || error.name === "TokenExpiredError")
