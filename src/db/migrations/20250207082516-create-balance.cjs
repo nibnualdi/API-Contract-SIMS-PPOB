939db0f20ce0ea869d6d7ffa0335f9e4215f6bcb
nibnualdi <nibnualdi@gmail.com>
2025-02-07 15:50:56 +0700

    add model & migration of balance

---
+++ src/db/migrations/20250207082516-create-balance.cjs
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Balances', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      balance: {
+        type: Sequelize.DECIMAL,
+        defaultValue: 0
+      },
+      membership_id: {
+        type: Sequelize.INTEGER,
+        unique: true,
+        references: {
+          model: 'memberships',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Balances');
+  }
+};
