28c2c6ba4341df1235c541b406f814a5a540f306
nibnualdi <nibnualdi@gmail.com>
2025-02-07 11:26:42 +0700

    add model & migration of banner

---
+++ src/db/migrations/20250207042439-create-banner.cjs
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Banners', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      banner_name: {
+        type: Sequelize.STRING
+      },
+      banner_image: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Banners');
+  }
+};
